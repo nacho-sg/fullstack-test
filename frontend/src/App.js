37798caeff457edfa1f8cd3ce581627ea032deb6
Nacho Sainz <nacho.sainz@nextiva.com>
2022-09-29 09:43:35 -0500

    Add frontend template

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <div class="header">Full Stack Interview Exercise</div>
+      <div class="main">Add your components here</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const javascriptAndReactRule = {
+  test: /\.[jt]sx?$/,
+  use: 'babel-loader',
+  exclude: /node_modules/,
+};
+
+const sassRule = {
+  test: /\.scss$/,
+  use: ['style-loader', 'css-loader', 'sass-loader'],
+};
+const cssRule = {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader'],
+};
+
+const configuration = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.js'),
+  devServer: {
+    port: 3000,
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [javascriptAndReactRule, sassRule, cssRule],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
+  ],
+};
+
+module.exports = configuration;
